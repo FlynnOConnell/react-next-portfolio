09a7efcc59a208163b927107fba7423c746d0196
Flynn <oconnell@binghamton.edu>
2022-10-23 18:18:28 -0400

    Add imgs and typing

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,9 +10,9 @@ export default function Home() {
         <div className="flex flex-col justify-center items-start max-w-2xl border-gray-200 dark:border-gray-700 mx-auto pb-16">
           <div className="flex flex-col-reverse sm:flex-row items-start">
             <div className="flex flex-col pr-8">
-              <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-1 text-black dark:text-white">
-                Flynn OConnell
-              </h1>
+
+            <img src="/signature_black.svg" className="filter-white"/>
+
               <h2 className="text-gray-700 dark:text-gray-200 mb-4">
                 Software Developer and Neuroscientist
               </h2>
